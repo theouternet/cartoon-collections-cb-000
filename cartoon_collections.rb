b1e87b20b73e3126ebc723043d6cd4d61eecfb21
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-12 22:32:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,7 @@ end
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    cheddar_cheese.include
-    
-  
+  return cheese if cheddar_cheese.include?(cheese) == true
+end
 
 end
